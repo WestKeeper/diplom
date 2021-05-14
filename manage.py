43eb9c1c9158fd7134c882d434933c18c6fec81e
Gallad <Gallad@gal.con>
2021-05-14 21:20:11 +0700

    data_func is added

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudentAnalytics.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VKR_webapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
